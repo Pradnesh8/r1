864388a4407bf606097e4b4ca1bb4f362dd0c61f
Pradnesh Khedekar <48099829+Pradnesh8@users.noreply.github.com>
2020-01-17 17:11:48 +0530

    ass3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Moment from 'react-moment';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,
+     Row, Col, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-   
+        
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
         function RenderDish({dish}) {
             console.log(dish)
             if (dish != null)
@@ -38,6 +46,87 @@ import { Link } from 'react-router-dom';
                     <div></div>
                 );
         }
+        
+        class CommentForm extends Component{
+            constructor(props){
+                super(props);
+
+                this.toggleModal = this.toggleModal.bind(this);
+                this.handleSubmit = this.handleSubmit.bind(this);
+                this.state = {
+                    isNavOpen: false,
+                    isModalOpen: false
+                };
+            }
+              toggleModal() {
+                this.setState({
+                  isModalOpen: !this.state.isModalOpen
+                });
+              }
+        
+              handleSubmit(values) {
+                console.log('Current State is: ' + JSON.stringify(values));
+                alert('Current State is: ' + JSON.stringify(values));
+                // event.preventDefault();
+              }
+            
+            render(){
+                return(
+                    <div>
+                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Submit Comment</Button>                        
+                        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                            <ModalBody>
+                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="rating" md={2}><strong>Rating</strong></Label>
+                                <Control.select model=".rating" name="rating" className="form-control m-2">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="firstname" className="m-2">Your Name</Label>
+                                    <Control.text model=".firstname" id="firstname" name="firstname"
+                                        placeholder="Name"
+                                        className="form-control m-2"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                         />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".firstname"
+                                        show="touched"
+                                        messages={{
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                     />
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="message" md={2}>Comment</Label>
+                                    <Control.textarea model=".message" id="message" name="message"
+                                        rows="6"
+                                        className="form-control m-2" />
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={2}>
+                                    <Button type="submit" color="primary">
+                                    Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                            </ModalBody>
+                        </Modal>
+                    </div>
+                );
+            }
+        }
 
         const DishDetail = (props) =>{
             console.log(props.dish);
@@ -46,7 +135,6 @@ import { Link } from 'react-router-dom';
                     <div className="container">
                     <div className="row">
                         <Breadcrumb>
-
                             <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
                             <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
                         </Breadcrumb>
@@ -61,6 +149,7 @@ import { Link } from 'react-router-dom';
                         </div>
                         <div className="col-12 col-md-5 m-1">
                             <RenderComments comments={props.comments} />
+                    <CommentForm />
                         </div>
                     </div>
                     </div>

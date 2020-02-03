2ca053e0602c9297840cc51bc6b9e7e0f32a22be
Pradnesh Khedekar <48099829+Pradnesh8@users.noreply.github.com>
2020-02-03 21:04:01 +0530

    react animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -18,13 +19,19 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val
             console.log(dish)
             if (dish != null)
                 return(
+                    <FadeTransform
+                        in
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
                     <Card>
                         <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
-                          <CardTitle>{dish.name}</CardTitle>
-                          <CardText>{dish.description}</CardText>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
                         </CardBody>
                     </Card>
+                    </FadeTransform>
                 );
             else
                 return(
@@ -34,18 +41,22 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val
         function RenderComments({comments, postComment, dishId}) {
             if (comments != null)
                 return(
+                    <Stagger in>
                     <div>{
                         comments.map((comment =>{
                             return (
+                            <Fade in>
                             <ul className="list-unstyled">
                             <li key={comment.id}></li>
                             <li className="list-unstyled mt-4">{comment.comment}</li>
                             <li className="list-unstyled mt-4">-- {comment.author}, <Moment format='MMM DD, YYYY'>{comment.date}</Moment></li>
-                            </ul>    
+                            </ul>
+                            </Fade>    
                         );
                     }))}
                         <CommentForm dishId={dishId} postComment={postComment} />   
                     </div>
+                    </Stagger>
                 );
             else
                 return(
